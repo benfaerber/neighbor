e3ed818b175d9adeb5144fffd136a133cdc67277
Ben Faerber <benfaerber@protonmail.com>
2025-11-13 19:37:20 -0700

    Add another health check

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,22 @@ struct ServiceHealth {
     message: String,
 } 
 
-#[get("/")]
-async fn index() -> impl Responder {
+fn health_response() -> impl Responder {
     HttpResponse::Ok().json(ServiceHealth {
         is_healthy: true,
         message: "Service is healthy! Send a post request to /search".into()
     })
 }
 
+#[get("/")]
+async fn index() -> impl Responder {
+    health_response()
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    health_response()
+}
 
 #[post("/search")]
 async fn search(request: web::Json<SearchRequest>) -> impl Responder {
@@ -56,7 +64,7 @@ async fn main() -> std::io::Result<()> {
     // They are probably gonna time me based on API response time so I will preload now.
     let _ = AllListings::get(); 
 
-    HttpServer::new(|| App::new().service(index).service(search))
+    HttpServer::new(|| App::new().service(index).service(search).service(health))
         .bind((IP_ADDRESS, PORT))?
         .run()
         .await
